68b5da7eb4f622d4ae333a0087bfd710e8b9c283
Gareth Powell <gmmapowell@yahoo.com>
2013-11-08 17:24:10 -0500

    Made the revised version work.

--- unicorn/expense-report/basic/actor/actor.js
+++ unicorn/expense-report/basic/actor/actor.js
@@ -1,18 +1,18 @@
-var UnicornProxyThinger = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin);
+var PromiseObjectProxy = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin);
 
 var actor = Ember.Object.extend({
   figureTotal: function(){
-    var envolopes = this.get('viewables').map(function(viewable){
+    var envelopes = this.get('viewables').map(function(viewable){
       return viewable.horn.receipt.as();
     });
 
-    var total = Ember.RSVP.all(envolopes).then(function(envolopes){
-      return values.reduce(function (accumulator, value) {
-        return accumulator + value;
+    var total = Ember.RSVP.all(envelopes).then(function(envelopes){
+      return envelopes.reduce(function (accumulator, value) {
+        return accumulator + Ember.get(value, 'amount');
       }, 0);
     });
 
-    return UnicornProxyThinger.create({
+    return PromiseObjectProxy.create({
       promise: total
     });
   }.property('viewables.@each.horn')
