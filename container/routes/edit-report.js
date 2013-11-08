c5bd96c1f9711538e78ffce3489eca2875a51c85
Stefan Penner <stefan.penner@gmail.com>
2013-11-08 15:31:01 -0500

    refactor edit-report-route model hook

--- container/routes/edit-report.js
+++ container/routes/edit-report.js
@@ -5,10 +5,14 @@ var EditReportRoute = Ember.Route.extend({
     return rs.promise;
   },
   model: function() {
-    var store = this.get('container').lookup('store:main');
-    return new Ember.RSVP.Promise(function(resolve) {
-      resolve({report: store.find('report', 17), receipts: Em.A([store.find('receipt', 19)])});
-    });
+    var store = this.store;
+
+    return {
+      report: store.find('report', 17),
+      receipts: [
+        store.find('receipt', 19)
+      ]
+    };
   },
   renderTemplate: function() {
     this.render('show-unicorn', {outlet: 'main'});
