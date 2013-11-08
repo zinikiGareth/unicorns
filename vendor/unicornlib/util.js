e7cd01ef1a64dda37f1a0e7fbc156fc2691be4fe
Stefan Penner <stefan.penner@gmail.com>
2013-11-08 16:16:12 -0500

    refactor embody

--- vendor/unicornlib/util.js
+++ vendor/unicornlib/util.js
@@ -3,56 +3,57 @@ import UnicornGoring from 'unicornlib/unicornGoring';
 import UnicornEnvelope from 'unicornlib/unicornEnvelope';
 
 function Util(Oasis, oasis) {
-  this.embody = function(emberContainer, mode, heart) {
+  this.embody = function (emberContainer, mode, heart) {
     var self = this;
-    // resolve promise (if any)
-    if (heart instanceof Ember.RSVP.Promise) {
-      return new Ember.RSVP.Promise(function(resolver) {
-        heart.then(function(resolved) { resolver(self.embody(mode, resolved)); });
-      })
-      return;
-    }
-    
-    if (mode == 'sandbox') {
-      var name = heart.get('unicorn');
-      
-      // somehow we need to get the list of unicorn capabilities
-      // this can be inferred from the unicorn code, but we DO NOT want to load that here
-      // Ideally, the tooling would have figured it out for us
 
-      // At the very least we should be able to find from the resolver:
-//      var contracts = container.lookup('capabilities:' + name);
-      
-      // for now, just hard code it ...
-      var contracts = ['_load', '_render', 'receiptEnvelope'];
-
-      var hash = this.createOasisSandbox(name, heart, contracts);
-      // and ask it to render itself ...
-      hash.horn.render.render();
-      return new Oasis.RSVP.Promise(function(resolver) {
-        resolver(hash);
-      });
-    } else if (mode == 'goring') {
-      var name = heart.get('unicorn');
-      var goring = emberContainer.lookup("unicorn:" + name);
-      return new Oasis.RSVP.Promise(function(resolver) {
-        goring.promise.then(function(code) {
+    return RSVP.resolve(heart).then(_embody);
+     
+    function _embody(heart) {
+      if (mode === 'sandbox') {
+        var name = heart.get('unicorn');
+        
+        // somehow we need to get the list of unicorn capabilities
+        // this can be inferred from the unicorn code, but we DO NOT want to load that here
+        // Ideally, the tooling would have figured it out for us
+  
+        // At the very least we should be able to find from the resolver:
+        // var contracts = container.lookup('capabilities:' + name);
+        
+        // for now, just hard code it ...
+        var contracts = ['_load', '_render', 'receiptEnvelope'];
+  
+        var hash = self.createOasisSandbox(name, heart, contracts);
+        // and ask it to render itself ...
+        hash.horn.render.render();
+        return RSVP.resolve(hash);
+      } else if (mode === 'goring') {
+  
+        var name = heart.get('unicorn');
+        var goring = emberContainer.lookup("unicorn:" + name);
+  
+        return goring.promise.then(function(code) {
           // TODO: both of these are wrong ... we should implement & call the load contract
           // with the heart id & possibly "state" info
           code.set('heart', heart);
-          code.render().then(function(view) {
+          return code.render().then(function(view) {
             // TODO: need "horn"
-            resolver(UnicornGoring.create({nestedView: view, heart: heart}));
+            return UnicornGoring.create({
+              nestedView: view,
+              heart: heart
+            });
           });
         });
-      });
-    } else if (mode == 'envelope') {
-      var envtemplate = emberContainer.lookup("template:envelopes/" + this.get('envelope'));
-      var ret = UnicornEnvelope.create({template: envtemplate, heart: heart});
-      // TODO: need some kind of "horn"
-      return new Oasis.RSVP.Promise(function(resolver) { resolver(ret)});
-    } else
-      throw new Error("Cannot handle the unicorn mode " + this.get('mode'));
+      } else if (mode === 'envelope') {
+        var envtemplate = emberContainer.lookup("template:envelopes/" + self.get('envelope'));
+        var ret = UnicornEnvelope.create({
+          template: envtemplate,
+          heart: heart
+        });
+        // TODO: need some kind of "horn"
+        return RSVP.resolve(ret);
+      } else
+        throw new Error("Cannot handle the unicorn mode " + self.get('mode'));
+    }
   };
 
   this.createOasisSandbox = function(name, heart, caps) {
