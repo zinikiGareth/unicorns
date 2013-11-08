dba1b8b30589a2feb2c5f67717bf611c47b9dc50
Stefan Penner <stefan.penner@gmail.com>
2013-11-08 15:49:30 -0500

    notes and ideas for resolving/fetching unicorns

--- container/routes/edit-report.js
+++ container/routes/edit-report.js
@@ -7,6 +7,16 @@ var EditReportRoute = Ember.Route.extend({
   model: function() {
     var store = this.store;
 
+    // sync report model find + summon unicorns
+    // - this likely replaces beforeModel, and augments
+    //   this model code below
+    //
+    // store.find('report', 17).then(function(model){
+    //   return fetchUncironFor(model.name).then(function(){
+    //     return model;
+    //   });
+    // })
+
     return {
       report: store.find('report', 17),
       receipts: [
