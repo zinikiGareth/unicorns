a80273a1f511efa7377d25c5211ce8bf34736f2b
Gareth Powell <gareth@ziniki.org>
2013-11-14 08:00:08 -0500

    Commented oddity of needing container etc. in view.

--- unicorn/expenseReport/basic/implements/render.js
+++ unicorn/expenseReport/basic/implements/render.js
@@ -5,11 +5,15 @@ var render = RenderContract.implement({
     render: function() {
       var self = this;
       return new Ember.RSVP.Promise(function (resolve) {
-        debugger;
         resolve(Ember.View.create({
           actor: self.get('actor'),
-          container: self.get('application').__container__,
+
+          // it is not clear to me why these need to be defined and it doesn't
+          // happen automatically.
+          // But in the sandbox-contains-goring case, this needs to be defined.
           context: {},
+          container: self.get('application').__container__,
+          
           template:MyTemplate,
           heart:self.get('heart'),
           viewables:self.get('viewables')
